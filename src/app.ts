0b4097e3e2e938b22c4c74b063cde4176e1e4ab8
IRADUKUNDA SANGWA CEDRIC <110623461+Dawaic6@users.noreply.github.com>
2024-03-15 14:24:59 +0200

    express practic

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,23 @@
-import express  from "express";
+import express, {Request ,Response}  from "express";
+import routes from "./routes";
 
 const app = express();
 
-app.get("/",(req, res) =>{
-    return res.send("hello world");
+app.use(express.json());
+//@ts-ignore
+ const middleware = 
+ ({name}:{name: string}) => 
+ (req: Request,res:Response, next: NextFunction) =>{
+//@ts-ignore
+    req.name =name;  
+    
+    next();
+ };
 
-});
+ app.use(middleware({name: 'TomDoesTech'}));
+
+ routes(app);
 
 app.listen(3000, () =>{
-    console.log('Appliction listening at http://local:3000');
+    console.log('Appliction listening at http://localhost:3000');
 });
---
+++ src/routes.ts
@@ -0,0 +1,8 @@
+import { Express,Request,Response,NextFunction } from "express";
+
+function routes(app: Express){
+    app.get( "/api/books/:bookId/:authorId", getBookHandler);
+    
+}
+
+export default routes;
